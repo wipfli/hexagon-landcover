81329b92ce9b63d81ba583e7eb4061ff64c27543
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2023-10-25 23:53:25 +0200

    Add demo page

--- test3.py
+++ test3.py
@@ -9,12 +9,51 @@ import gzip
 
 import time
 
+import sys
+
+# first argument of script is the zoom level.
+# second argument of script is the filename.
+#
+# Example z=19 and filename=clipped-esa.tif:
+#
+# python3 test3.py 19 clipped-esa.tif
+#
+
+z = int(sys.argv[1])
+print('z =', z)
+
+filename = sys.argv[2]
+print('filename =', filename)
+
+# map from pixel value to index for summing, color, and name
+classes = {
+    10: { 'index': 0, 'color': '0,100,0,255', 'name': 'Tree cover' },
+    20: { 'index': 1, 'color': '255,187,34,255', 'name': 'Shrubland' },
+    30: { 'index': 2, 'color': '255,255,76,255', 'name': 'Grassland' },
+    40: { 'index': 3, 'color': '240,150,255,255', 'name': 'Cropland' },
+    50: { 'index': 4, 'color': '250,0,0,255', 'name': 'Built-up' },
+    60: { 'index': 5, 'color': '180,180,180,255', 'name': 'Bare / sparse vegetation' },
+    70: { 'index': 6, 'color': '240,240,240,255', 'name': 'Snow and Ice' },
+    80: { 'index': 7, 'color': '0,100,200,255', 'name': 'Permanent water bodies' },
+    90: { 'index': 8, 'color': '0,150,160,255', 'name': 'Herbaceous wetland' },
+    95: { 'index': 9, 'color': '0,207,117,255', 'name': 'Mangroves' },
+    100: { 'index': 10, 'color': '250,230,160,255', 'name': 'Moss and lichen' },
+}
+
+classes_index_to_value = {
+}
+for c in classes:
+    classes_index_to_value[classes[c]['index']] = c
+
+# for c in classes:
+#     print(f'{c},')
+#     print(f'"rgba({classes[c]["color"]})"')
 
 def get_x_max(z):
     return 2 ** z
 
 def get_triangle(x, y, z):
-    x_max = 2 ** z
+    x_max = get_x_max(z)
     i = floor(x)
     j = floor(y)
 
@@ -39,7 +78,7 @@ def get_triangle(x, y, z):
             else:
                 x_triangle = i + 1
         else: # i is even
-            if (x - i) > (y -j):
+            if (x - i) > (y - j):
                 x_triangle = i + 1
             else:
                 x_triangle = i
@@ -287,12 +326,12 @@ def process_image(filename, z):
                     int(pixel_values[i][j]): 1
                 }
 
-            # # Print the progress
-            # pixels_processed = (i * pixel_values.shape[1]) + j + 1
-            # total_pixels = pixel_values.shape[0] * pixel_values.shape[1]
-            # if pixels_processed % 10000 == 0:
-            #     progress = pixels_processed / total_pixels
-            #     print(f"{filename}: {progress:.2%} complete", end="\r")
+            # Print the progress
+            pixels_processed = (i * pixel_values.shape[1]) + j + 1
+            total_pixels = pixel_values.shape[0] * pixel_values.shape[1]
+            if pixels_processed % 10000 == 0:
+                progress = pixels_processed / total_pixels
+                print(f"{filename}: {progress:.2%} complete", end="\r")
     
     print('total time', time.time() - tic)
     hexagons_majority = {}
@@ -323,21 +362,49 @@ def process_image(filename, z):
     gdf.to_file(filename='polygon.gpkg', driver="GPKG")
             
 
-def process_image_csv(filename, z):
+def step_1(filename, z):
+
+    # compute the x_triangle, y_triangle values for each pixel and write them to a csv file with the pixel value
+    # produces a csv file which looks like this:
+    # 
+    # x_triangle, y_triangle, value
+    # 51886, 19110, 70
+    # 51886, 19110, 70
+    # ...
+
+    tic = time.time()
 
     # Open the geotiff file
-    dataset = gdal.Open(filename)
+    dataset = gdal.Open(f'downloads/{filename}')
     
     # Get the pixel values and the geotransform information
     band = dataset.GetRasterBand(1)
     nodata = band.GetNoDataValue()
     geotransform = dataset.GetGeoTransform()
-    pixel_values = band.ReadAsArray().astype(float)
+    # pixel_values = band.ReadAsArray().astype(float)
+    pixel_values = band.ReadAsArray().astype(np.uint8)
     
+    longitudes = [geotransform[0] + (j + 0.5) * geotransform[1] + (0 + 0.5) * geotransform[2] for j in range(pixel_values.shape[1])]
+    latitudes = [geotransform[3] + (0 + 0.5) * geotransform[4] + (i + 0.5) * geotransform[5] for i in range(pixel_values.shape[0])]
 
-    tic = time.time()
+    # print('longitudes', longitudes[0], longitudes[-1])
+    # print('latitudes', latitudes[0], latitudes[-1])
 
-    with gzip.open('output.csv.gz', 'wt') as f:
+    xs = []
+    for longitude in longitudes:
+        x, _ = get_x_y(lat=0, lon=longitude, z=z)
+        xs.append(x)
+    
+    ys = []
+    for latitude in latitudes:
+        _, y = get_x_y(lat=latitude, lon=0, z=z)
+        ys.append(y)
+    
+    # print('xs', xs[0], xs[-1])
+    # print('ys', ys[0], ys[-1])
+
+    # with gzip.open(f'output_step_1/{filename}-triangles-z{z}.csv.gz', 'wt') as f:
+    with open(f'output_step_1/{filename}-triangles-z{z}.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['x_triangle', 'y_triangle', 'value'])
             
@@ -346,23 +413,139 @@ def process_image_csv(filename, z):
                 # Skip nodata pixels
                 if pixel_values[i][j] == nodata:
                     continue
-                            
-                # Convert the pixel coordinates to map coordinates
-                lon = geotransform[0] + (j + 0.5) * geotransform[1] + (i + 0.5) * geotransform[2]
-                lat = geotransform[3] + (j + 0.5) * geotransform[4] + (i + 0.5) * geotransform[5]
 
-                x, y = get_x_y(lat, lon, z)
+                x, y = xs[j], ys[i]
                 x_triangle, y_triangle = get_triangle(x, y, z)
+
                 writer.writerow([x_triangle, y_triangle, int(pixel_values[i][j])])
+
                 # Print the progress
                 pixels_processed = (i * pixel_values.shape[1]) + j + 1
                 total_pixels = pixel_values.shape[0] * pixel_values.shape[1]
-                if pixels_processed % 10000 == 0:
+                if pixels_processed % 1000000 == 0:
                     progress = pixels_processed / total_pixels
-                    print(f"{filename}: {progress:.2%} complete", end="\r")
+                    print(f"step_1 {progress:.2%} complete")
 
-    print('total time', time.time() - tic)
-         
+    # print(x_triangles)
+    print('step_1', time.time() - tic)
 
-process_image_csv(filename='ESA_WorldCover_10m_2020_v100_N66W039_Map.tif', z=17)
-# process_image_csv(filename='clipped-esa.tif', z=17)
+
+def step_2(filename, z):
+
+    # sum the triangle values from step 1 and creates a triangle counts csv file
+    # where triangles only appear once
+    # example csv file:
+    # 
+    # x_triangle,y_triangle,count_index_0,count_index_1,count_index_2,count_index_3,count_index_4,count_index_5,count_index_6,count_index_7,count_index_8,count_index_9,count_index_10
+    # 51849,19102,0,0,0,0,0,0,0,501,0,0,0
+    # 51850,19102,0,0,0,0,0,0,0,386,0,0,0
+    # 51851,19102,0,0,0,0,0,0,0,667,0,0,0
+    # 51852,19102,0,0,0,0,0,0,0,387,0,0,0
+    # ...
+
+    tic = time.time()
+
+    triangles = {}
+
+    # with gzip.open(f'output_step_1/{filename}-triangles-z{z}.csv.gz', 'rt') as f:
+    with open(f'output_step_1/{filename}-triangles-z{z}.csv', 'r') as f:
+        reader = csv.reader(f)
+        i = 0
+        next(reader) # skip header line
+        for row in reader:
+            x_triangle, y_triangle, value = [int(element) for element in row]
+            
+            if (x_triangle, y_triangle) not in triangles:            
+                triangles[(x_triangle, y_triangle)] = [0 for _ in range(11)]
+            
+            index = classes[value]['index']
+            triangles[(x_triangle, y_triangle)][index] += 1
+
+    # with gzip.open(f'output_step_2/{filename}-triangle-counts-z{z}.csv.gz', 'wt') as f:
+    with open(f'output_step_2/{filename}-triangle-counts-z{z}.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            'x_triangle',
+            'y_triangle',
+            'count_index_0',
+            'count_index_1',
+            'count_index_2',
+            'count_index_3', 
+            'count_index_4',
+            'count_index_5',
+            'count_index_6',
+            'count_index_7',
+            'count_index_8',
+            'count_index_9',
+            'count_index_10'
+        ])
+        for triangle in triangles:
+            x_triangle, y_triangle = triangle
+            row = [x_triangle, y_triangle] + triangles[(x_triangle, y_triangle)]
+            writer.writerow(row)
+
+    print('step_2', time.time() - tic)
+
+def step_3(filename, z):
+
+    # turns the triangles from step 2 into hexagons with a single value
+
+    tic = time.time()
+
+    hexagons = {}
+
+    # with gzip.open(f'output_step_2/{filename}-triangle-counts-z{z}.csv.gz', 'rt') as f:
+    with open(f'output_step_2/{filename}-triangle-counts-z{z}.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # skip header line
+        for row in reader:
+            elements = [int(element) for element in row]
+            x_triangle, y_triangle = elements[0], elements[1]
+            counts = elements[2:]
+            
+            x_hexagon, y_hexagon = get_hexagon(x_triangle, y_triangle)
+
+            if (x_hexagon, y_hexagon) not in hexagons:            
+                hexagons[(x_hexagon, y_hexagon)] = counts
+            else:
+                for i in range(len(counts)):
+                    hexagons[(x_hexagon, y_hexagon)][i] += counts[i]
+    
+    hexagons_majority = {}
+    for key in hexagons:
+        max_index = hexagons[key].index(max(hexagons[key]))
+        hexagons_majority[key] = classes_index_to_value[max_index]
+    
+    data = {
+        'value': [],
+        'geometry': []
+    }
+
+    for key in hexagons_majority:
+        triangles = get_triangles(key[0], key[1])
+        for triangle in triangles:
+            data['value'].append(hexagons_majority[key])
+            data['geometry'].append(get_polygon(triangle[0], triangle[1], z))
+    
+    gdf = gpd.GeoDataFrame(data, crs='epsg:4326')
+    gdf = gdf.dissolve('value')
+    gdf.to_file(filename=f'output_step_3/{filename}-polygon-z{z}.gpkg', driver="GPKG")
+
+    print('step_3', time.time() - tic)
+
+
+
+
+# process_image(filename='clipped-esa.tif', z=17)
+
+#step_1(filename='ESA_WorldCover_10m_2020_v100_N66W039_Map.tif', z=17)
+
+# filename = 'clipped-esa.tif'
+# filename = 'ESA_WorldCover_10m_2020_v100_N45E006_Map.tif'
+step_1(filename=filename, z=z)
+step_2(filename=filename, z=z)
+step_3(filename=filename, z=z)
+
+# process_image_np(filename='ESA_WorldCover_10m_2020_v100_N66W039_Map.tif', z=17)
+# process_image_np(filename='clipped-esa.tif', z=17)
+
